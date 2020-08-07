1ddd8f836ce2f60e1ed621446fe54246edb9168d
Minh Vo <56403104+minhdvo1703@users.noreply.github.com>
2020-08-07 00:16:49 -0400

    Delete The PAD.sql

--- SQL/The PAD.sql
+++
@@ -1,2 +0,0 @@
-SELECT CONCAT(NAME,'(',SUBSTR(occupation,1,1),')') FROM OCCUPATIONS ORDER BY NAME ASC;
-SELECT CONCAT('There are a total of ', COUNT(occupation), ' ', LOWER(occupation),'s.') FROM OCCUPATIONS GROUP BY occupation ORDER BY COUNT(occupation)
